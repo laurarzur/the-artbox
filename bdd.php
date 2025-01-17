b973cd72c9c64c3756e920cde030880244b45ad9
laurarzur <laura.arzur@gmail.com>
2025-01-17 16:32:40 +0100

    gestion du formulaire

--- bdd.php
+++ bdd.php
@@ -3,3 +3,15 @@
     function connection() {
         return new PDO('mysql:dbname=artbox;host=localhost', 'root', '');
     }
+
+   /**
+    * Fonction qui nettoie les entrées utilisateurs en enlevant les espaces inutiles 
+    * et en changeant les tags en entités html
+    * @param string $input entrée utilisateur à nettoyer
+    * @return string entrée utilisateur nettoyée
+    */
+    function sanitize($input) : string {
+        $output = trim($input); 
+        $output = htmlspecialchars($output); 
+        return $output;
+    }
