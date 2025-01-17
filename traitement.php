3c836c4ec079379e0e02dca65530372af7b260c0
laurarzur <laura.arzur@gmail.com>
2025-01-17 17:59:17 +0100

    supression du else

--- traitement.php
+++ traitement.php
@@ -5,29 +5,29 @@
     || strlen($_POST['description']) < 3 
     || !filter_var($_POST['img'], FILTER_VALIDATE_URL)) 
     {
-        header('Location: ajouter.php');
+        header('Location: ajouter.php'); 
+        exit();
         
-    } else {
+    }
 
-        require 'bdd.php'; 
-    
-        $name = sanitize($_POST['name']); 
-        $artist = sanitize($_POST['artist']); 
-        $img = sanitize($_POST['img']); 
-        $description = sanitize($_POST['description']);
-    
-        $bdd = connection();
-        $sql = "INSERT INTO oeuvres (name, artist, img, description) VALUES (:name, :artist, :img, :description)"; 
-    
-        $query = $bdd->prepare($sql); 
-        $query->bindParam(":name", $name, PDO::PARAM_STR); 
-        $query->bindParam(":artist", $artist, PDO::PARAM_STR);
-        $query->bindParam(":img", $img, PDO::PARAM_STR);
-        $query->bindParam(":description", $description, PDO::PARAM_STR); 
-    
-        if ($query->execute()) {
-            header('Location: oeuvre.php?id=' . $bdd->lastInsertId());
-        }
+    require 'bdd.php'; 
+
+    $name = sanitize($_POST['name']); 
+    $artist = sanitize($_POST['artist']); 
+    $img = sanitize($_POST['img']); 
+    $description = sanitize($_POST['description']);
+
+    $bdd = connection();
+    $sql = "INSERT INTO oeuvres (name, artist, img, description) VALUES (:name, :artist, :img, :description)"; 
+
+    $query = $bdd->prepare($sql); 
+    $query->bindParam(":name", $name, PDO::PARAM_STR); 
+    $query->bindParam(":artist", $artist, PDO::PARAM_STR);
+    $query->bindParam(":img", $img, PDO::PARAM_STR);
+    $query->bindParam(":description", $description, PDO::PARAM_STR); 
+
+    if ($query->execute()) {
+        header('Location: oeuvre.php?id=' . $bdd->lastInsertId());
     }
 
 
